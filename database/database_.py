8b5f3d20ca1847b58675b386644d751926398377
Vlad_shayzi1 <tecnomega@mail.ru>
2024-06-02 10:41:17 +0300

    Бот готов к хостингу.

--- database/database_.py
+++ database/database_.py
@@ -9,7 +9,7 @@ from disnake import Guild
 class DatabaseCommands:
      
      async def create_table_for_new_guild(self, guild: Guild) -> bool:
-          async with aiosqlite.connect('data/discord.db') as db:
+          async with aiosqlite.connect('/data/discord.db') as db:
                await db.execute("""CREATE TABLE IF NOT EXISTS server{}(
                     id INT,
                     once TEXT,
@@ -30,7 +30,7 @@ class DatabaseCommands:
      
      
      async def new_sound_save(self, guild: Guild, data: list[str]) -> None:
-          async with aiosqlite.connect('data/discord.db') as db:
+          async with aiosqlite.connect('/data/discord.db') as db:
                cursor = await db.execute("SELECT panel FROM server{}".format(guild.id))
                cursor = await cursor.fetchone()
                cursor: list[str] = json.loads(cursor[0])
@@ -46,7 +46,7 @@ class DatabaseCommands:
                
                
      async def get_panel(self, guild: Guild) -> list[list[str]]:
-          async with aiosqlite.connect('data/discord.db') as db:
+          async with aiosqlite.connect('/data/discord.db') as db:
                cursor = await db.execute("SELECT panel FROM server{}".format(guild.id))
                cursor = await cursor.fetchone()
                cursor: list[list[str]] = json.loads(cursor[0])
@@ -55,7 +55,7 @@ class DatabaseCommands:
           
           
      async def panel_save(self, guild: Guild, panel: list[list[str]]) -> None:
-          async with aiosqlite.connect('data/discord.db') as db:
+          async with aiosqlite.connect('/data/discord.db') as db:
                await db.execute("UPDATE server{} SET panel = ?".format(guild.id), [json.dumps(panel)])
                await db.commit()
                
